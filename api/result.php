db5e58b596bcda698397005613c8614e45df8d31
Jonathan <Jonathan.webitb@gmail.com>
2019-02-18 10:51:56 -0300

    gi

--- api/result.php
+++ api/result.php
@@ -1,45 +1,47 @@
 <?php
 sleep(1);
 ?>
-<link rel="stylesheet" type="text/css" href="../css/style.css" />
-<table class="tb-list" id="result">
-    <tr data-aos="fade-left"
-        data-aos-anchor="#example-anchor"
-        data-aos-offset="400"
-        data-aos-duration="1100">
-        <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
-        <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
-        <td class="td-contrato">332851</td>
-        <td class="td-data">13/02/2019</td>
-    </tr>
-    <tr>
-        <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
-        <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
-        <td class="td-contrato">332851</td>
-        <td class="td-data">13/02/2019</td>
-    </tr>
-    <tr>
-        <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
-        <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
-        <td class="td-contrato">332851</td>
-        <td class="td-data">13/02/2019</td>
-    </tr>
-    <tr>
-        <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
-        <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
-        <td class="td-contrato">332851</td>
-        <td class="td-data">13/02/2019</td>
-    </tr>
-    <tr>
-        <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
-        <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
-        <td class="td-contrato">332851</td>
-        <td class="td-data">13/02/2019</td>
-    </tr>
-    <tr>
-        <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
-        <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
-        <td class="td-contrato">332851</td>
-        <td class="td-data">13/02/2019</td>
-    </tr>
-</table>
+<div class="listtb"  data-aos="fade-left"
+     data-aos-anchor="#example-anchor"
+     data-aos-offset="400"
+     data-aos-duration="400">
+    
+    <table class="tb-list" id="result">
+        <tr>
+            <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
+            <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
+            <td class="td-contrato">332851</td>
+            <td class="td-data">13/02/2019</td>
+        </tr>
+        <tr>
+            <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
+            <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
+            <td class="td-contrato">332851</td>
+            <td class="td-data">13/02/2019</td>
+        </tr>
+        <tr>
+            <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
+            <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
+            <td class="td-contrato">332851</td>
+            <td class="td-data">13/02/2019</td>
+        </tr>
+        <tr>
+            <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
+            <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
+            <td class="td-contrato">332851</td>
+            <td class="td-data">13/02/2019</td>
+        </tr>
+        <tr>
+            <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
+            <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
+            <td class="td-contrato">332851</td>
+            <td class="td-data">13/02/2019</td>
+        </tr>
+        <tr>
+            <td class="td-icon"><img src="../images/editar_contrato.png" class="img-icon-list" alt="contrato-list"></th>
+            <td class="td-desc"><div class="td-desc-list">Prefeitura Barueri aaaaaaaaaaa</div></td>
+            <td class="td-contrato">332851</td>
+            <td class="td-data">13/02/2019</td>
+        </tr>
+    </table>
+</div>
