0417e262cac448027e9d0f36bf2be897dae6aa55
Jonathan <Jonathan.webitb@gmail.com>
2019-04-04 09:59:59 -0300

    gited

--- includes/menu.php
+++ includes/menu.php
@@ -1,6 +1,6 @@
 <ul class="menu">
     <li><a class="active" href="includes/home.php" target="iframe" id="home">Home</a></li>
-    <li><a href="includes/meus_contrato.php" target="iframe" id="meus_contrato">Meus Contratos</a></li>
+    <li><a href="includes/meus_contratos.php" target="iframe" id="meus_contrato">Meus Contratos</a></li>
     <li><a href="apresentacao/cadastraContrato.php" target="iframe" id="administracao">Administração</a></li>
 </ul>  
 <ul class="sair">
---
+++ includes/meus_contratos.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="../css/style.css" />
+        <link rel="icon" href="favicon.png">
+        <title></title>
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"/>
+
+        <script src="../js/jquery.min.js" type="text/javascript"></script>
+
+        <script src="../js/jquery.maskMoney.js"></script>
+
+    </head>
+    <body>
+
+        <div id="list" data-aos="zoom-in" >
+            <div class="container-list">
+                <div class="title-list">
+                    <label class="desc-title-list">Meus Contratos</label>
+                </div>
+                <div class="title-list-tb">
+
+                    <div class="div-icon"></div>
+                    <div class="div-desc">Descrição</div>
+                    <div class="div-contrato">Contrato</div>
+                    <div class="div-data">Data</div>
+                </div>
+                <div class="table-list" id="result">
+                    <!--CONTEUDO-->
+
+                </div>
+            </div>
+
+        </div>       
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <script>
+            AOS.init();
+        </script>
+
+    </body>
+
+</html>
